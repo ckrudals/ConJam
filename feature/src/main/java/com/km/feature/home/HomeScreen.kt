f90354d516695fae0b6f2ecefa7e62352338d634
KyeongMIn <goodluckplz820@gmail.com>
2025-08-25 21:54:42 +0900

    Implement [] LazyVerticalGrid을 사용하여 콘서트 목록 리스트 구현

--- feature/src/main/java/com/km/feature/home/HomeScreen.kt
+++ feature/src/main/java/com/km/feature/home/HomeScreen.kt
@@ -2,12 +2,16 @@ package com.km.feature.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.core.model.Performance
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.ui.Alignment
+import coil.compose.AsyncImage
 
 @Composable
 fun HomeScreen(
@@ -22,11 +26,21 @@ fun HomeScreen(
 fun HomeScreen(
     performances: List<Performance>
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+    LazyVerticalGrid(
+        columns = androidx.compose.foundation.lazy.grid.GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        performances.forEach {
-            Text(text = it.title)
+        items(items = performances, key = { it.id }) {
+            Column(
+                horizontalAlignment =  Alignment.CenterHorizontally
+            ) {
+                AsyncImage(
+                    model = it.poster,
+                    contentDescription = null,
+                )
+                Text(text = it.title)
+            }
         }
     }
 }
